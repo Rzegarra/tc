0b5752a3b367041da21d7b4007ef6d6c97279267
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2015-11-25 12:00:52 -0500

    tablas
    aumenta fila y columna

--- tc grafos/index.js
+++ tc grafos/index.js
@@ -10,7 +10,7 @@ console.log('tamaño de filas = '+ rows);
 var columns=$('tr:first td').length;
 console.log('tamaño de columnas = '+ columns);
 var newColumn="<td><input type='text' id='dato'></td>"
-
+var newRow="<tr></tr>"
 function estados(obj) {
   for (var i = 1; i < obj.length; i++) {
     nodos[i-1]=obj[i][0];
@@ -25,7 +25,11 @@ function estados(obj) {
   // datos[0][2]=2
   // datos[0][3]=3
   // datos[1][1]=1
-
+  var rows=$('tr').length;
+  console.log('tamaño de filas = '+ rows);
+  var columns=$('tr:first td').length;
+  console.log('tamaño de columnas = '+ columns);
+  ///////
   var a=$(dato);
   var temp=0;
   for (var i = 0; i < rows; i++) {
@@ -52,4 +56,15 @@ $('#delCol').on('click',function(){
     $('tr:eq('+i+') td:last').remove();
   }
   console.log('entre');
+});
+$('#addRow').on('click',function(){
+  var rows=$('tr').length;
+  console.log('tamaño de filas = '+ rows);
+  var columns=$('tr:first td').length;
+  console.log('tamaño de columnas = '+ columns);
+  $('table').append(newRow)
+  for (var i = 0; i < columns; i++) {
+    $('tr:last').append("<td><input type='text' id='dato'></td>");
+  }
+
 });
